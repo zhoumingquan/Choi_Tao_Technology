5fccc5a5961fe41e8a8ef050c4742a7ee123f67c
zhoumingquan <zhoumingquan@ycs168.cn>
2018-12-17 09:26:01 +0800

    123

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name:{{ name }}</div>
-    <div class="dashboard-text">roles:<span v-for="role in roles" :key="role">{{ role }}</span></div>
+    <div class="dashboard-text">
+      <span>账户余额</span>
+      <span><el-button type="text">2832.08</el-button></span>
+      <span style="margin-left:30px;"><el-button type="text">充值</el-button></span>
+    </div>
+    <dir style="text-align: center;">
+      <span>您有<el-button type="text">2个</el-button>账单未支付，将会影响您的正常使用。</span>
+    </dir>
+    <div style="text-align: center;margin-top:200px;">
+      <el-button type="primary" round>订单查询</el-button>
+      <el-button type="info" round>账单查询</el-button>
+    </div>
   </div>
 </template>
 
@@ -23,9 +33,10 @@ export default {
 .dashboard {
   &-container {
     margin: 30px;
+    text-align: right;
   }
   &-text {
-    font-size: 30px;
+    font-size: 18px;
     line-height: 46px;
   }
 }
